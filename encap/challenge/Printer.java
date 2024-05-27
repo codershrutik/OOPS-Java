57d6138af5366903c5028ffbfc0e18fc83aa496d
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-05-27 09:44:52 +0000

    encap added

---
+++ encap/challenge/Printer.java
@@ -0,0 +1,12 @@
+public class Printer{
+
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex){
+        this.tonerLevel = tonerLevel;
+        this.duplex = duplex;
+        
+    }
+}
